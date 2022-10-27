6d9c0802a1f39870a2226a36d1e32e57b788fb73
Ramses Martinez <ramses.mtz96@gmail.com>
2022-10-26 20:07:35 -0500

    Listo proyecto vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Ejemplo de formulario</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
